99fc902947cc8318b56f55d8d016e62e471f96f0
mlucasdev <lucasmsantos819@gmail.com>
2023-10-27 16:26:52 -0300

    feat: ensure FetchQuoteUseCase return StockQuoteNotFoundError if FetchQuoteBySymbolApi returns null

--- src/interactions/usecases/fetch-quote-usecase.ts
+++ src/interactions/usecases/fetch-quote-usecase.ts
@@ -1,12 +1,16 @@
 import type { FetchQuote, FetchQuoteResponse } from '@/domain/contracts/fetch-quote'
 import type { FetchQuoteBySymbolApi } from '../contracts/api/fetch-quote-by-symbol-api'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
+import { StockQuoteNotFoundError } from '@/domain/errors/sotck-quote-not-found-error'
 
 export class FetchQuoteUseCase implements FetchQuote {
   constructor (private readonly fetchQuoteBySymbolApi: FetchQuoteBySymbolApi) {}
 
   async perform (stockSymbol: string): Promise<FetchQuoteResponse> {
-    await this.fetchQuoteBySymbolApi.fetchQuote(stockSymbol)
+    const stockQuote = await this.fetchQuoteBySymbolApi.fetchQuote(stockSymbol)
+    if (!stockQuote) {
+      return left(new StockQuoteNotFoundError(stockSymbol))
+    }
     return await Promise.resolve(
       right({ name: '', lastPrice: 0, pricedAt: '' })
     )
