60b9be036f3ac4d34ff9e2753cb4253b24757aa7
mlucasdev <lucasmsantos819@gmail.com>
2023-10-27 17:19:03 -0300

    feat: ensure FetchQuoteController call FetchQuote with correct stock symbol

--- src/presentation/controllers/fetch-quote-controller.ts
+++ src/presentation/controllers/fetch-quote-controller.ts
@@ -1,16 +1,21 @@
+import type { FetchQuote } from '@/domain/contracts/fetch-quote'
 import type { Controller } from '../contracts/controller'
 import type { Validation } from '../contracts/validation'
 import { badRequest } from '../helpers/http-helper'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class FetchQuoteController implements Controller {
-  constructor (private readonly validationStub: Validation) {}
+  constructor (
+    private readonly validationStub: Validation,
+    private readonly fetchQuote: FetchQuote
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const validationResult = await this.validationStub.validate(httpRequest.params)
     if (validationResult.isLeft()) {
       return badRequest(validationResult.value)
     }
+    await this.fetchQuote.perform(httpRequest.params.stockSymbol)
     return await Promise.resolve({
       body: '', statusCode: 0
     })
