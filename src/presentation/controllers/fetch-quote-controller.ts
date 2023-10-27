bb487b74d4ce1e51133bf8dcc2b0c52ecbd78f6a
mlucasdev <lucasmsantos819@gmail.com>
2023-10-27 17:04:24 -0300

    feat: ensure FetchQuoteController return 400 if Validation fails

--- src/presentation/controllers/fetch-quote-controller.ts
+++ src/presentation/controllers/fetch-quote-controller.ts
@@ -6,7 +6,13 @@ export class FetchQuoteController implements Controller {
   constructor (private readonly validationStub: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.validationStub.validate(httpRequest.params)
+    const validationResult = await this.validationStub.validate(httpRequest.params)
+    if (validationResult.isLeft()) {
+      return {
+        statusCode: 400,
+        body: validationResult.value
+      }
+    }
     return await Promise.resolve({
       body: '', statusCode: 0
     })
