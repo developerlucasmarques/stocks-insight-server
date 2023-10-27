729259dbc40a62bedd199fab49a3e54a3e9743dc
mlucasdev <lucasmsantos819@gmail.com>
2023-10-27 17:29:11 -0300

    feat: ensure FetchQuoteController return 500 if Validation throws

--- src/presentation/controllers/fetch-quote-controller.ts
+++ src/presentation/controllers/fetch-quote-controller.ts
@@ -1,7 +1,7 @@
 import type { FetchQuote } from '@/domain/contracts/fetch-quote'
 import type { Controller } from '../contracts/controller'
 import type { Validation } from '../contracts/validation'
-import { badRequest, notFound } from '../helpers/http-helper'
+import { badRequest, notFound, serverError } from '../helpers/http-helper'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class FetchQuoteController implements Controller {
@@ -11,16 +11,20 @@ export class FetchQuoteController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validationResult = await this.validationStub.validate(httpRequest.params)
-    if (validationResult.isLeft()) {
-      return badRequest(validationResult.value)
+    try {
+      const validationResult = await this.validationStub.validate(httpRequest.params)
+      if (validationResult.isLeft()) {
+        return badRequest(validationResult.value)
+      }
+      const fetchQuoteResult = await this.fetchQuote.perform(httpRequest.params.stockSymbol)
+      if (fetchQuoteResult.isLeft()) {
+        return notFound(fetchQuoteResult.value)
+      }
+      return await Promise.resolve({
+        body: '', statusCode: 0
+      })
+    } catch (error: any) {
+      return serverError(error)
     }
-    const fetchQuoteResult = await this.fetchQuote.perform(httpRequest.params.stockSymbol)
-    if (fetchQuoteResult.isLeft()) {
-      return notFound(fetchQuoteResult.value)
-    }
-    return await Promise.resolve({
-      body: '', statusCode: 0
-    })
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,3 +1,4 @@
+import { ServerError } from '../errors/server-error'
 import type { HttpResponse } from '../http-types/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
@@ -8,4 +9,9 @@ export const badRequest = (error: Error): HttpResponse => ({
 export const notFound = (error: Error): HttpResponse => ({
   statusCode: 404,
   body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })
