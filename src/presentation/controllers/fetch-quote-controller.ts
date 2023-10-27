4fcb725664bc354a8534748064c2257ee8908f1f
mlucasdev <lucasmsantos819@gmail.com>
2023-10-27 17:24:03 -0300

    feat: ensure FetchQuoteController return 404 if FetchQuote returns StockQuoteNotFoundError

--- src/presentation/controllers/fetch-quote-controller.ts
+++ src/presentation/controllers/fetch-quote-controller.ts
@@ -1,7 +1,7 @@
 import type { FetchQuote } from '@/domain/contracts/fetch-quote'
 import type { Controller } from '../contracts/controller'
 import type { Validation } from '../contracts/validation'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, notFound } from '../helpers/http-helper'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class FetchQuoteController implements Controller {
@@ -15,7 +15,10 @@ export class FetchQuoteController implements Controller {
     if (validationResult.isLeft()) {
       return badRequest(validationResult.value)
     }
-    await this.fetchQuote.perform(httpRequest.params.stockSymbol)
+    const fetchQuoteResult = await this.fetchQuote.perform(httpRequest.params.stockSymbol)
+    if (fetchQuoteResult.isLeft()) {
+      return notFound(fetchQuoteResult.value)
+    }
     return await Promise.resolve({
       body: '', statusCode: 0
     })
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,3 +4,8 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
+export const notFound = (error: Error): HttpResponse => ({
+  statusCode: 404,
+  body: error
+})
