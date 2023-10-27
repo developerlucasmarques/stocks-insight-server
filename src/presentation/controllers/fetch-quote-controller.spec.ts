af7afb20b6b6c34feb470b5e398a228e56ae2e30
mlucasdev <lucasmsantos819@gmail.com>
2023-10-27 17:04:35 -0300

    test: ensure FetchQuoteController return 400 if Validation fails

--- src/presentation/controllers/fetch-quote-controller.spec.ts
+++ src/presentation/controllers/fetch-quote-controller.spec.ts
@@ -1,4 +1,4 @@
-import { type Either, right } from '@/shared/either'
+import { type Either, right, left } from '@/shared/either'
 import type { Validation } from '../contracts/validation'
 import type { HttpRequest } from '../http-types/http'
 import { FetchQuoteController } from './fetch-quote-controller'
@@ -37,5 +37,17 @@ describe('FetchQuote Controller', () => {
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenCalledWith({ stockSymbol: 'any_stock_symbol' })
+  })
+
+  it('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.resolve(left(new Error('any_message')))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      body: new Error('any_message')
+    })
   })
 })
