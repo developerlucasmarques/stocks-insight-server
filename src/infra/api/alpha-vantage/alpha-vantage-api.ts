4d116459372bac1967cfd1414839ac6476c4d8aa
mlucasdev <lucasmsantos819@gmail.com>
2023-10-28 12:54:59 -0300

    feat: ensure AlphaVantageApi return a GlobalStockQuote if fetch stock quote is a success

--- src/infra/api/alpha-vantage/alpha-vantage-api.ts
+++ src/infra/api/alpha-vantage/alpha-vantage-api.ts
@@ -1,6 +1,7 @@
 import type { StockQuote } from '@/domain/models/stock-quote'
 import type { FetchStockQuoteBySymbolApi } from '@/interactions/contracts/api/fetch-stock-quote-by-symbol-api'
 import axios from 'axios'
+import type { GlobalStockQuote } from './types/global-stock-quote'
 
 export class AlphaVantageApi implements FetchStockQuoteBySymbolApi {
   private readonly baseUrl = 'https://www.alphavantage.co/query?function='
@@ -8,7 +9,12 @@ export class AlphaVantageApi implements FetchStockQuoteBySymbolApi {
 
   async fetchStockQuote (stockSymbol: string): Promise<null | StockQuote > {
     const url = this.baseUrl + `GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${this.apiKey}`
-    await axios.get(url)
-    return null
+    const response = await axios.get(url)
+    const data: GlobalStockQuote = response.data
+    return {
+      name: data['Global Quote']['01. symbol'],
+      lastPrice: Number(Number(data['Global Quote']['05. price']).toFixed(2)),
+      pricedAt: data['Global Quote']['07. latest trading day']
+    }
   }
 }
