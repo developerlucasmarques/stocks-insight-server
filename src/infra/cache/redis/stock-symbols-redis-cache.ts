930921ea0a84217db7976fcafb8add2fdc8ed3c5
mlucasdev <lucasmsantos819@gmail.com>
2023-10-28 11:12:42 -0300

    feat: ensure StockSymbolsRedisCache fetch one symbol on success

--- src/infra/cache/redis/stock-symbols-redis-cache.ts
+++ src/infra/cache/redis/stock-symbols-redis-cache.ts
@@ -1,9 +1,20 @@
 import type { AddAllStockSymbolsCache } from '@/interactions/contracts/cache/add-all-stock-symbols-cache'
 import { RedisHelper } from './helpers/redis-helper'
+import { type FetchStockSymbolCache, type StockSymbol } from '@/interactions/contracts/cache/fetch-stock-symbol-cache'
 
-export class StockSymbolsRedisCache implements AddAllStockSymbolsCache {
+export class StockSymbolsRedisCache implements AddAllStockSymbolsCache, FetchStockSymbolCache {
   async addAllSymbols (symbols: string[]): Promise<void> {
     const redis = RedisHelper.getInstance()
     await redis.set('stockSymbols', JSON.stringify(symbols))
+  }
+
+  async fetchOneSymbol (stockSymbol: string): Promise<null | StockSymbol> {
+    const redis = RedisHelper.getInstance()
+    const symbolsJson = await redis.get('stockSymbols')
+    if (!symbolsJson) return null
+    const allSymbols: string[] = JSON.parse(symbolsJson)
+    const symbol = allSymbols.find((symbol) => symbol === stockSymbol)
+    if (!symbol) return null
+    return { symbol }
   }
 }
