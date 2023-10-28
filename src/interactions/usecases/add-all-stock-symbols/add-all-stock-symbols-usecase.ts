f4109712269995e0e4659c4a10ead18e1d8b8cf9
mlucasdev <lucasmsantos819@gmail.com>
2023-10-28 16:16:59 -0300

    feat: ensure AddAllStockSymbolsUseCase call AddAllStockSymbolsCache with correct symbols

--- src/interactions/usecases/add-all-stock-symbols/add-all-stock-symbols-usecase.ts
+++ src/interactions/usecases/add-all-stock-symbols/add-all-stock-symbols-usecase.ts
@@ -1,18 +1,21 @@
 import type { AddAllStockSymbols, AddAllStockSymbolsResponse } from '@/domain/contracts/add-all-stock-symbols'
 import { StockSymbolsNotFoundError } from '@/domain/errors/stock-symbols-not-found-error'
 import type { FetchAllSymbolsOfListedStocksApi } from '@/interactions/contracts/api/fetch-all-symbols-of-listed-stocks-api'
+import type { AddAllStockSymbolsCache } from '@/interactions/contracts/cache/add-all-stock-symbols-cache'
 import { left, right } from '@/shared/either'
 
 export class AddAllStockSymbolsUseCase implements AddAllStockSymbols {
   constructor (
-    private readonly fetchAllSymbols: FetchAllSymbolsOfListedStocksApi
+    private readonly fetchAllSymbolsOfListedStocksApi: FetchAllSymbolsOfListedStocksApi,
+    private readonly addAllStockSymbolsCache: AddAllStockSymbolsCache
   ) {}
 
   async perform (): Promise<AddAllStockSymbolsResponse> {
-    const symbols = await this.fetchAllSymbols.fetchAll()
+    const symbols = await this.fetchAllSymbolsOfListedStocksApi.fetchAll()
     if (!symbols) {
       return left(new StockSymbolsNotFoundError())
     }
+    await this.addAllStockSymbolsCache.addAllSymbols(symbols.symbols)
     return right(null)
   }
 }
