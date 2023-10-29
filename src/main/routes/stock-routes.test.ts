c6b7bb04853de44198d542288ffddd83b34d9d87
mlucasdev <lucasmsantos819@gmail.com>
2023-10-29 17:23:09 -0300

    refactor: connect to redis before integration tests

--- src/main/routes/stock-routes.test.ts
+++ src/main/routes/stock-routes.test.ts
@@ -1,7 +1,25 @@
-import app from '@/main/config/app'
+import { RedisHelper } from '@/infra/cache/redis/helpers/redis-helper'
+import type { Redis } from 'ioredis'
+import RedisMock from 'ioredis-mock'
 import request from 'supertest'
+import app from '../config/app'
 
-describe('Stock Routes', () => {
+let redis: Redis
+
+describe('StockSymbolsRedis Cache', () => {
+  beforeAll(() => {
+    redis = new RedisMock()
+    jest.spyOn(RedisHelper, 'getInstance').mockReturnValue(redis)
+  })
+
+  afterAll(() => {
+    redis.disconnect()
+  })
+
+  beforeEach(async () => {
+    await redis.flushall()
+  })
+
   it('Should return the current an stock quote', async () => {
     const stockSymbol = 'any_stock_symbol'
     await request(app)
