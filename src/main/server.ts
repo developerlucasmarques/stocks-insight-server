f4bfca44b02a5500d15a02285b42f5715a76819b
mlucasdev <lucasmsantos819@gmail.com>
2023-10-29 18:43:47 -0300

    feat: save stock symbols in cache before starting the server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,9 @@
 import 'module-alias/register'
 import app from './config/app'
 import env from './config/envs/server-env'
+import { addAllStockSymbolsUseCaseFactory } from './factories/usecases/add-all-stock-symbols-usecase-factory'
+
+addAllStockSymbolsUseCaseFactory()
 
 app.listen(env.port, () => {
   console.log(`Server running at http://${env.host}:${env.port}`)
