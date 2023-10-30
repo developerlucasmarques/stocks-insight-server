97e7ef9b1656855f0df1e94cbd4894ac21690036
mlucasdev <lucasmsantos819@gmail.com>
2023-10-30 12:01:30 -0300

    test: ensure RedisHelper connect Redis server with correct configs

--- src/infra/cache/redis/helpers/redis-helper.spec.ts
+++ src/infra/cache/redis/helpers/redis-helper.spec.ts
@@ -14,4 +14,12 @@ describe('RedisHelper', () => {
     const redis2 = RedisHelper.getInstance()
     expect(redis1).toBe(redis2)
   })
+
+  it('Should connect Redis server with correct configs', () => {
+    const redis = RedisHelper.getInstance()
+    const config = redis.options
+    expect(config.host).toBe('0.0.0.0')
+    expect(config.port).toBe(6379)
+    expect(config.password).toBe('')
+  })
 })
