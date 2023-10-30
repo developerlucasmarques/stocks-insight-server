a9dfd5ad6fc807cb5d334290769ac246e72bed84
mlucasdev <lucasmsantos819@gmail.com>
2023-10-30 20:02:29 -0300

    feat: connect to mongodb before starting the server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,17 @@
 import 'module-alias/register'
-import app from './config/app'
-import env from './config/envs/server-env'
 import { RedisHelper } from '@/infra/cache/redis/helpers/redis-helper'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import app from './config/app'
+import mongodbEnv from './config/envs/mongodb-env'
+import env from './config/envs/server-env'
 import { addAllStockSymbolsUseCaseFactory } from './factories/usecases/add-all-stock-symbols-usecase-factory'
 
 RedisHelper.connect()
 const addAllStockSymbolsUseCase = addAllStockSymbolsUseCaseFactory()
 
-addAllStockSymbolsUseCase.perform()
-  .then(() => {
+MongoHelper.connect(mongodbEnv.mongoUrl)
+  .then(async () => {
+    await addAllStockSymbolsUseCase.perform()
     app.listen(env.port, () => {
       console.log(`Server running at http://${env.host}:${env.port}`)
     })
