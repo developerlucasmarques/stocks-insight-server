24ce2bdcf168f79adbadd4e927152421ac5d5882
mlucasdev <lucasmsantos819@gmail.com>
2023-10-30 10:31:25 -0300

    fix: mock the return of RedisHelper getInstance method for RedisMemoryServer

--- src/main/routes/stock-routes.test.ts
+++ src/main/routes/stock-routes.test.ts
@@ -2,6 +2,7 @@ import { Redis } from 'ioredis'
 import { RedisMemoryServer } from 'redis-memory-server'
 import request from 'supertest'
 import app from '../config/app'
+import { RedisHelper } from '@/infra/cache/redis/helpers/redis-helper'
 
 let redisServer: RedisMemoryServer
 let redis: Redis
@@ -12,6 +13,7 @@ describe('Stock Routes', () => {
     const host = await redisServer.getHost()
     const port = await redisServer.getPort()
     redis = new Redis({ host, port })
+    jest.spyOn(RedisHelper, 'getInstance').mockReturnValue(redis)
   })
 
   afterAll(async () => {
@@ -24,7 +26,7 @@ describe('Stock Routes', () => {
   })
 
   it('Should return the current an stock quote', async () => {
-    await redis.set('stockSymbols', JSON.stringify(['AAPL', 'IBM']))
+    await redis.set('stockSymbols', JSON.stringify(['AAPL', 'TSLA']))
     await request(app)
       .get('/stock/AAPL/quote')
       .expect(200)
