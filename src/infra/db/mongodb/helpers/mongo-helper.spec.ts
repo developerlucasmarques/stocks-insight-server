c1546e87d907efb4ea362bc451cd320538bd3717
mlucasdev <lucasmsantos819@gmail.com>
2023-10-30 14:32:41 -0300

    test: ensure MongoHelper set a default URI when uri is undefined

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -1,21 +1,25 @@
 import { Collection } from 'mongodb'
 import { MongoHelper as sut } from './mongo-helper'
-import env from '@/main/config/envs/mongodb-env'
+
+let anyCollection: Collection
 
 describe('Mongo Helper', () => {
-  beforeEach(async () => {
-    await sut.connect(env.mongoUrl)
-  })
-
   afterEach(async () => {
     await sut.disconnect()
   })
 
   it('Should reconnect if MongoDB is down', async () => {
-    let anyCollection = await sut.getCollection('any')
+    anyCollection = await sut.getCollection('any')
     expect(anyCollection).toBeInstanceOf(Collection)
     await sut.disconnect()
     anyCollection = await sut.getCollection('any')
     expect(anyCollection).toBeInstanceOf(Collection)
+  })
+
+  it('Should set a default URI when uri is undefined', async () => {
+    const uri = undefined
+    await sut.connect(uri)
+    const currentUri = sut.getUri()
+    expect(currentUri).toBe('mongodb://127.0.0.1:27017/stocks-insight')
   })
 })
