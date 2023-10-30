6af46fceffa8c829763a4ddf72eaa2688150ecef
mlucasdev <lucasmsantos819@gmail.com>
2023-10-29 22:02:10 -0300

    feat: add all stock symbols in redis cache before server start

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,14 @@
 import 'module-alias/register'
 import app from './config/app'
 import env from './config/envs/server-env'
+import { RedisHelper } from '@/infra/cache/redis/helpers/redis-helper'
 import { addAllStockSymbolsUseCaseFactory } from './factories/usecases/add-all-stock-symbols-usecase-factory'
 
-addAllStockSymbolsUseCaseFactory()
-
-app.listen(env.port, () => {
-  console.log(`Server running at http://${env.host}:${env.port}`)
-})
+RedisHelper.connect()
+addAllStockSymbolsUseCaseFactory().perform()
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`Server running at http://${env.host}:${env.port}`)
+    })
+  })
+  .catch(console.error)
