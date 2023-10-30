419505147275bee9b1fa20e949449f3a0790953e
mlucasdev <lucasmsantos819@gmail.com>
2023-10-30 13:06:08 -0300

    test: ensure LogControllerDecorator call the Controller handle method with the correct values

---
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -0,0 +1,39 @@
+import type { Controller } from '@/presentation/contracts'
+import { ok } from '@/presentation/helpers/http/http-helper'
+import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
+import { LogControllerDecorator } from './log-controller-decorator'
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return await Promise.resolve(ok({ data: 'any_value' }))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    field: 'any_value'
+  }
+})
+
+type SutTypes = {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return { sut, controllerStub }
+}
+
+describe('LogController Decorator', () => {
+  it('Should call the Controller handle method with the correct values', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    await sut.handle(makeFakeRequest())
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
