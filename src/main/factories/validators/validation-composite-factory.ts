334ab735f5015bab4a21cbbeeadecc7c199b2c22
mlucasdev <lucasmsantos819@gmail.com>
2023-10-30 20:39:59 -0300

    refactor: rename folder validation-composite to composite

--- src/main/factories/validators/validation-composite-factory.ts
+++ src/main/factories/validators/validation-composite-factory.ts
@@ -1,5 +1,5 @@
 import type { Validation } from '@/presentation/contracts'
-import { ValidationComposite } from '@/presentation/helpers/validators/validation-compose/validation-composite'
+import { ValidationComposite } from '@/presentation/helpers/validators/composite/validation-composite'
 
 export const validationCompositeFactory = (validations: Validation[]): Validation => {
   return new ValidationComposite(validations)
