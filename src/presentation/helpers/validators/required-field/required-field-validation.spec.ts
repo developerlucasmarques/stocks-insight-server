d8c6a6e62b69579f54b9472f4ded0532896a2ec2
mlucasdev <lucasmsantos819@gmail.com>
2023-10-30 20:23:25 -0300

    test: ensure RequiredFieldValidation return a MissinParamError if field is empty on input

--- src/presentation/helpers/validators/required-field/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field/required-field-validation.spec.ts
@@ -1,10 +1,23 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '@/presentation/errors'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
 describe('RequiredField Validation', () => {
   it('Should return a MissinParamError if input not contain field name', async () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const result = await sut.validate({ name: 'any name' })
+    expect(result.value).toEqual(new MissingParamError('field'))
+  })
+
+  it('Should return a MissinParamError if field is empty on input', async () => {
+    const sut = makeSut()
+    const result = await sut.validate({
+      name: 'any name',
+      field: ''
+    })
     expect(result.value).toEqual(new MissingParamError('field'))
   })
 })
