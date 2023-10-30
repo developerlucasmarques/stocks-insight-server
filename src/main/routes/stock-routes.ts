2c52e89ec588e4d4d1e61627751150832ecb25f6
mlucasdev <lucasmsantos819@gmail.com>
2023-10-29 22:02:34 -0300

    feat: ensure Stock Quote route return the current an stock quote

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -1,8 +1,9 @@
 import type { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { fetchStockQuoteControllerFactory } from '../factories/controllers/fetch-stock-quote-controller-factory'
 
 export default (router: Router): void => {
-  router.get('/stock/:stockSymbol/quote', (req, res) => {
-    const stockSymbol = req.params.stockSymbol
-    res.json({ ok: stockSymbol })
-  })
+  router.get(
+    '/stock/:stockSymbol/quote', adaptRoute(fetchStockQuoteControllerFactory())
+  )
 }
