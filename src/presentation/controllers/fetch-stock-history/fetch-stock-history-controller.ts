7622d4298604f3a468f288427d38c915eb8bd3e4
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:17:57 -0300

    feat: ensure FetchStockHistoryController return 400 if Validation fails

--- src/presentation/controllers/fetch-stock-history/fetch-stock-history-controller.ts
+++ src/presentation/controllers/fetch-stock-history/fetch-stock-history-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import type { Controller, Validation } from '../../contracts'
 import type { HttpRequest, HttpResponse } from '../../http-types/http'
 
@@ -5,7 +6,10 @@ export class FetchStockHistoryController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.validation.validate(httpRequest.params)
+    const validationResult = await this.validation.validate(httpRequest.params)
+    if (validationResult.isLeft()) {
+      return badRequest(validationResult.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }
