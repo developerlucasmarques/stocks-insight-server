ad9f8bdfca990a13b582f77e41c0a5b8ab249f8f
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 08:57:22 -0300

    feat: ensure AlphaVantageApi fetchStockHistory() call axios with correct url

--- src/infra/api/alpha-vantage/alpha-vantage-api.ts
+++ src/infra/api/alpha-vantage/alpha-vantage-api.ts
@@ -1,13 +1,22 @@
 import type { StockQuote } from '@/domain/models/stock-quote'
-import type { FetchStockQuoteBySymbolApi } from '@/interactions/contracts/api'
+import type { FetchStockHistoryApi, FetchStockQuoteBySymbolApi } from '@/interactions/contracts/api'
 import axios from 'axios'
 import { MaximumLimitReachedError } from './errors/maximun-limit-reached-error'
 import type { GlobalStockQuote } from './types/global-stock-quote'
+import { type StockHistory } from '@/domain/models/stock-history'
+import { type FetchStockHistoryData } from '@/domain/contracts'
 
-export class AlphaVantageApi implements FetchStockQuoteBySymbolApi {
+export class AlphaVantageApi implements FetchStockQuoteBySymbolApi, FetchStockHistoryApi {
   private readonly baseUrl = 'https://www.alphavantage.co/query?function='
 
   constructor (private readonly apiKey: string) {}
+
+  private makeUrl (func: string, symbol: string, outputsize?: string): string {
+    if (outputsize) {
+      return `${this.baseUrl}${func}&symbol=${symbol}&outputsize=${outputsize}&apikey=${this.apiKey}`
+    }
+    return `${this.baseUrl}${func}&symbol=${symbol}&apikey=${this.apiKey}`
+  }
 
   async fetchStockQuote (stockSymbol: string): Promise<null | StockQuote > {
     const url = this.makeUrl('GLOBAL_QUOTE', stockSymbol)
@@ -26,7 +35,9 @@ export class AlphaVantageApi implements FetchStockQuoteBySymbolApi {
     }
   }
 
-  private makeUrl (func: string, symbol: string): string {
-    return `${this.baseUrl}${func}&symbol=${symbol}&apikey=${this.apiKey}`
+  async fetchStockHistory (data: FetchStockHistoryData): Promise<null | StockHistory> {
+    const url = this.makeUrl('TIME_SERIES_DAILY', data.stockSymbol, 'full')
+    await axios.get(url)
+    return null
   }
 }
---
+++ src/infra/api/alpha-vantage/types/daily-stock-quote.ts
@@ -0,0 +1,7 @@
+export type DailyStockQuote = Record<string, {
+  '1. open': string
+  '2. high': string
+  '3. low': string
+  '4. close': string
+  '5. volume': string
+}>
