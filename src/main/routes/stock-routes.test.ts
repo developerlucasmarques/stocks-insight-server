a4fc112f12a85b5a62ce4a8e0df69de18b708577
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 20:41:51 -0300

    test: ensure stocks/:stockSymbol/comparison route return 200 if StockComparison on success

--- src/main/routes/stock-routes.test.ts
+++ src/main/routes/stock-routes.test.ts
@@ -7,6 +7,8 @@ import { RedisHelper } from '@/infra/cache/redis/helpers/redis-helper'
 let redisServer: RedisMemoryServer
 let redis: Redis
 
+jest.setTimeout(10000)
+
 describe('Stock Routes', () => {
   beforeAll(async () => {
     redisServer = new RedisMemoryServer()
@@ -36,6 +38,13 @@ describe('Stock Routes', () => {
     await redis.set('stockSymbols', JSON.stringify(['AAPL', 'TSLA']))
     await request(app)
       .get('/stocks/AAPL/history?from=2023-01-02&to=2023-01-03')
+      .expect(200)
+  })
+
+  it('Should return 200 if StockComparison on success', async () => {
+    await redis.set('stockSymbols', JSON.stringify(['AAPL', 'TSLA', 'AMZN']))
+    await request(app)
+      .get('/stocks/AAPL/comparison?stocksToCompare=TSLA,AMZN')
       .expect(200)
   })
 })
