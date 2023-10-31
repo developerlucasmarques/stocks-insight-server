12352acdeafaff307fffe5c8e2b4ed386ebd5767
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 14:29:44 -0300

    test: ensure FetchStockComparisonUseCase call FetchManyStockQuotesBySymbolsApi with correct stock symbols

--- src/interactions/usecases/fetch-stock-comparison/fetch-stock-comparison-usecase.spec.ts
+++ src/interactions/usecases/fetch-stock-comparison/fetch-stock-comparison-usecase.spec.ts
@@ -1,41 +1,44 @@
 import type { StockQuote } from '@/domain/models/stock-quote'
-import type { FetchStockQuoteBySymbolApi } from '@/interactions/contracts/api'
+import { type FetchManyStockQuotesBySymbolsApi } from '@/interactions/contracts/api/fetch-many-stock-quotes-by-symbols-api'
 import { FetchStockComparisonUseCase } from './fetch-stock-comparison-usecase'
 
 const makeFakeStockSymbols = (): string[] => (['any_stock', 'another_stock'])
 
-const makeFakeStockQuote = (): StockQuote => ({
+const makeFakeStockQuotes = (): StockQuote[] => ([{
   name: 'any_stock',
   lastPrice: 120.99,
   pricedAt: 'any_priced_at'
-})
+}, {
+  name: 'another_stock',
+  lastPrice: 133.99,
+  pricedAt: 'any_priced_at'
+}])
 
-const makeFetchStockQuoteBySymbolApi = (): FetchStockQuoteBySymbolApi => {
-  class FetchStockQuoteBySymbolApiStub implements FetchStockQuoteBySymbolApi {
-    async fetchStockQuote (stockSymbol: string): Promise<null | StockQuote> {
-      return await Promise.resolve(makeFakeStockQuote())
+const makeFetchManyStockQuotesBySymbolsApi = (): FetchManyStockQuotesBySymbolsApi => {
+  class FetchManyStockQuotesBySymbolsApiStub implements FetchManyStockQuotesBySymbolsApi {
+    async fetchManyStockQuotes (stockSymbols: string[]): Promise<null | StockQuote[]> {
+      return await Promise.resolve(makeFakeStockQuotes())
     }
   }
-  return new FetchStockQuoteBySymbolApiStub()
+  return new FetchManyStockQuotesBySymbolsApiStub()
 }
 
 type SutTypes = {
   sut: FetchStockComparisonUseCase
-  fetchStockQuoteBySymbolApiStub: FetchStockQuoteBySymbolApi
+  fetchManyStockQuotesBySymbolsApiStub: FetchManyStockQuotesBySymbolsApi
 }
 
 const makeSut = (): SutTypes => {
-  const fetchStockQuoteBySymbolApiStub = makeFetchStockQuoteBySymbolApi()
-  const sut = new FetchStockComparisonUseCase(fetchStockQuoteBySymbolApiStub)
-  return { sut, fetchStockQuoteBySymbolApiStub }
+  const fetchManyStockQuotesBySymbolsApiStub = makeFetchManyStockQuotesBySymbolsApi()
+  const sut = new FetchStockComparisonUseCase(fetchManyStockQuotesBySymbolsApiStub)
+  return { sut, fetchManyStockQuotesBySymbolsApiStub }
 }
 
 describe('FetchStockComparison UseCase', () => {
-  it('Should call FetchStockQuoteBySymbolApi with correct stock symbols', async () => {
-    const { sut, fetchStockQuoteBySymbolApiStub } = makeSut()
-    const fetchStockQuoteSpy = jest.spyOn(fetchStockQuoteBySymbolApiStub, 'fetchStockQuote')
+  it('Should call FetchManyStockQuotesBySymbolsApi with correct stock symbols', async () => {
+    const { sut, fetchManyStockQuotesBySymbolsApiStub } = makeSut()
+    const fetchStockQuoteSpy = jest.spyOn(fetchManyStockQuotesBySymbolsApiStub, 'fetchManyStockQuotes')
     await sut.perform(makeFakeStockSymbols())
-    expect(fetchStockQuoteSpy).toHaveBeenCalledWith('any_stock')
-    expect(fetchStockQuoteSpy).toHaveBeenCalledWith('another_stock')
+    expect(fetchStockQuoteSpy).toHaveBeenCalledWith(makeFakeStockSymbols())
   })
 })
