b05aa7044e8c27764077187501c40e13b74b8cb2
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:16:34 -0300

    test: ensure FetchStockHistoryController call Validation with correct value

---
+++ src/presentation/controllers/fetch-stock-history/fetch-stock-history-controller.spec.ts
@@ -0,0 +1,45 @@
+import { right, type Either } from '@/shared/either'
+import type { Validation } from '../../contracts'
+import type { HttpRequest } from '../../http-types/http'
+import { FetchStockHistoryController } from './fetch-stock-history-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    stockSymbol: 'any_stock_symbol',
+    from: '2023-01-02',
+    to: '2023-01-03'
+  }
+})
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    async validate (input: any): Promise<Either<Error, null>> {
+      return await Promise.resolve(right(null))
+    }
+  }
+  return new ValidationStub()
+}
+
+type SutTypes = {
+  sut: FetchStockHistoryController
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new FetchStockHistoryController(validationStub)
+  return { sut, validationStub }
+}
+
+describe('FetchStockHistory Controller', () => {
+  it('Should call Validation with correct value', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    await sut.handle(makeFakeRequest())
+    expect(validateSpy).toHaveBeenCalledWith({
+      stockSymbol: 'any_stock_symbol',
+      from: '2023-01-02',
+      to: '2023-01-03'
+    })
+  })
+})
