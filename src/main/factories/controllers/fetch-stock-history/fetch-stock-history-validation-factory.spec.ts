f367c629c0c71abf5585eb1e245286805568800a
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 10:43:02 -0300

    test: ensure FetchStockHistoryValidation Factory call ValidationComposite with all validations

---
+++ src/main/factories/controllers/fetch-stock-history/fetch-stock-history-validation-factory.spec.ts
@@ -0,0 +1,21 @@
+import type { Validation } from '@/presentation/contracts'
+import { stockSymbolValidationFactory } from '../../validators/stock-symbol-validation-factory'
+import { ValidationComposite } from '@/presentation/helpers/validators/composite/validation-composite'
+import { fetchStockHistoryValidationFactory } from './fetch-stock-history-validation'
+import { dateFormatValidationFactory } from '../../validators/date-format-validation-factory'
+import { dateRangeValidationFactory } from '../../validators/date-range-validation-factory'
+
+jest.mock('@/presentation/helpers/validators/composite/validation-composite')
+
+describe('FetchStockHistoryValidation Factory', () => {
+  it('Should call ValidationComposite with all validations', () => {
+    fetchStockHistoryValidationFactory()
+    const validations: Validation[] = []
+    validations.push(
+      stockSymbolValidationFactory(),
+      dateFormatValidationFactory(['to', 'from']),
+      dateRangeValidationFactory('to', 'from')
+    )
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
