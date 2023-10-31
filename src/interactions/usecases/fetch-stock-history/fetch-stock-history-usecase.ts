24a58f62a53f077b2a18354728eea165550cff53
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 06:57:39 -0300

    feat: ensure FetchStockHistoryUseCase return StockHistoryNotFoundError if FetchStockHistoryApi returns null

--- src/interactions/usecases/fetch-stock-history/fetch-stock-history-usecase.ts
+++ src/interactions/usecases/fetch-stock-history/fetch-stock-history-usecase.ts
@@ -1,12 +1,16 @@
 import type { FetchStockHistory, FetchStockHistoryData, FetchStockHistoryResponse } from '@/domain/contracts/fetch-stock-history'
+import { StockHistoryNotFoundError } from '@/domain/errors/stock-history-not-found-error'
 import type { FetchStockHistoryApi } from '@/interactions/contracts/api'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
 
 export class FetchStockHistoryUseCase implements FetchStockHistory {
   constructor (private readonly fetchStockHistoryApi: FetchStockHistoryApi) {}
 
   async perform (data: FetchStockHistoryData): Promise<FetchStockHistoryResponse> {
-    await this.fetchStockHistoryApi.fetchStockHistory(data)
+    const stockHistory = await this.fetchStockHistoryApi.fetchStockHistory(data)
+    if (!stockHistory) {
+      return left(new StockHistoryNotFoundError(data))
+    }
     return right({
       name: '',
       prices: []
