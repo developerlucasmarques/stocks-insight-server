a09add1a09014cffcd56317181313efa92f1f794
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:52:25 -0300

    test: ensure DateParamValidation return InvalidDateError for date with invalid format (non-numeric characters)

--- src/presentation/helpers/validators/date-param/date-param-validation.spec.ts
+++ src/presentation/helpers/validators/date-param/date-param-validation.spec.ts
@@ -18,4 +18,11 @@ describe('DateParam Validation', () => {
     expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023_01_02'))
   })
+
+  it('Should return InvalidDateError for date with invalid format (non-numeric characters)', async () => {
+    const sut = makeSut()
+    const result = await sut.validate({ to: '2023-Jan-02' })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toEqual(new InvalidDateError('2023-Jan-02'))
+  })
 })
