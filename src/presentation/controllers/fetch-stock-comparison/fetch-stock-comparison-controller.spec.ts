a3b69b9bdcbbb19e964bf03f43b0d3fff5c774b5
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 14:55:33 -0300

    test: ensure FetchStockComparison return 400 if Params Validation fails

--- src/presentation/controllers/fetch-stock-comparison/fetch-stock-comparison-controller.spec.ts
+++ src/presentation/controllers/fetch-stock-comparison/fetch-stock-comparison-controller.spec.ts
@@ -1,7 +1,8 @@
-import { right, type Either } from '@/shared/either'
+import { right, type Either, left } from '@/shared/either'
 import type { Validation } from '../../contracts'
 import type { HttpRequest } from '../../http-types/http'
 import { FetchStockComparisonController } from './fetch-stock-comparison-controller'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -38,5 +39,14 @@ describe('FetchStockComparison Controller', () => {
     const validateSpy = jest.spyOn(paramsValidationStub, 'validate')
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenCalledWith({ stockSymbol: 'any_stock_symbol' })
+  })
+
+  it('Should return 400 if Params Validation fails', async () => {
+    const { sut, paramsValidationStub } = makeSut()
+    jest.spyOn(paramsValidationStub, 'validate').mockReturnValueOnce(
+      Promise.resolve(left(new Error('any_message')))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error('any_message')))
   })
 })
