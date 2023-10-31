ed760fcab6cbd9aec4dbf37ac029f4c63fe0d3ed
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:27:12 -0300

    feat: ensure FetchStockHistoryController call FetchStockHistory with correct values

--- src/presentation/controllers/fetch-stock-history/fetch-stock-history-controller.ts
+++ src/presentation/controllers/fetch-stock-history/fetch-stock-history-controller.ts
@@ -1,9 +1,13 @@
 import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import type { Controller, Validation } from '../../contracts'
 import type { HttpRequest, HttpResponse } from '../../http-types/http'
+import type { FetchStockHistory } from '@/domain/contracts'
 
 export class FetchStockHistoryController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly fetchStockHistory: FetchStockHistory
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -11,6 +15,8 @@ export class FetchStockHistoryController implements Controller {
       if (validationResult.isLeft()) {
         return badRequest(validationResult.value)
       }
+      const { stockSymbol, from: initialDate, to: finalDate } = httpRequest.params
+      await this.fetchStockHistory.perform({ stockSymbol, initialDate, finalDate })
       return { statusCode: 0, body: '' }
     } catch (error: any) {
       return serverError(error)
