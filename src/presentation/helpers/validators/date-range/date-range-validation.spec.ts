e9e51c0174e99331f653f999fe09b018ec457bd1
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 08:31:17 -0300

    test: ensure DateRangeValidation return InvalidDateRangeError if initial date is greater than final date

---
+++ src/presentation/helpers/validators/date-range/date-range-validation.spec.ts
@@ -0,0 +1,18 @@
+import { InvalidDateRangeError } from '@/presentation/errors'
+import { DateRangeValidation } from './date-range-validation'
+
+const makeFakeInput = (initialDate: string, finalDate: string): any => (
+  { to: initialDate, from: finalDate }
+)
+
+const makeSut = (): DateRangeValidation => {
+  return new DateRangeValidation('to', 'from')
+}
+
+describe('DateRange Validation', () => {
+  it('Should return InvalidDateRangeError if initial date is greater than final date', async () => {
+    const sut = makeSut()
+    const result = await sut.validate(makeFakeInput('2023-01-03', '2023-01-02'))
+    expect(result.value).toEqual(new InvalidDateRangeError())
+  })
+})
