c5f512214f189f5cb60aeb4430af5d953b509aea
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 20:03:18 -0300

    refactor: move FetchStockQuoteController Factory to fetch-stock-quote folder

--- src/main/factories/controllers/fetch-stock-quote/fetch-stock-quote-controller-factory.ts
+++ src/main/factories/controllers/fetch-stock-quote/fetch-stock-quote-controller-factory.ts
@@ -1,8 +1,8 @@
 import type { Controller } from '@/presentation/contracts'
 import { FetchStockQuoteController } from '@/presentation/controllers'
-import { stockSymbolValidationFactory } from '../validators'
-import { fetchStockQuoteUseCaseFactory } from '../usecases'
-import { logControllerDecoratorFactory } from '../decorators/log-controller-decorator-factory'
+import { stockSymbolValidationFactory } from '../../validators'
+import { fetchStockQuoteUseCaseFactory } from '../../usecases'
+import { logControllerDecoratorFactory } from '../../decorators/log-controller-decorator-factory'
 
 export const fetchStockQuoteControllerFactory = (): Controller => {
   const controller = new FetchStockQuoteController(
--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -1,6 +1,6 @@
 import type { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
-import { fetchStockQuoteControllerFactory } from '../factories/controllers/fetch-stock-quote-controller-factory'
+import { fetchStockQuoteControllerFactory } from '../factories/controllers/fetch-stock-quote/fetch-stock-quote-controller-factory'
 import { fetchStockStoryControllerFactory } from '../factories/controllers/fetch-stock-history/fetch-stock-history-controller-factory'
 
 export default (router: Router): void => {
