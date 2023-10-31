c50435a5c00735b9e71048469f75dbb9b2e73680
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 15:37:04 -0300

    feat: ensure FetchStockComparisonController call FetchStockHistory with correct values

--- src/presentation/controllers/fetch-stock-comparison/fetch-stock-comparison-controller.ts
+++ src/presentation/controllers/fetch-stock-comparison/fetch-stock-comparison-controller.ts
@@ -1,29 +1,36 @@
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 import type { Controller, Validation } from '@/presentation/contracts'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
-import { type Either } from '@/shared/either'
+import type { Either } from '@/shared/either'
+import type { FetchStockComparison } from '@/domain/contracts'
 
 export class FetchStockComparisonController implements Controller {
   constructor (
     private readonly paransValidation: Validation,
-    private readonly queryValidation: Validation
+    private readonly queryValidation: Validation,
+    private readonly fetchStockComparison: FetchStockComparison
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const paramsValidationResult = await this.paransValidation.validate(httpRequest.params)
-    if (paramsValidationResult.isLeft()) {
-      return badRequest(paramsValidationResult.value)
-    }
-    const validationsResult: Array<Either<Error, null>> = []
-    const stocksToCompare: string[] = httpRequest.query?.stocksToCompare.split(',')
-    for (const stock of stocksToCompare) {
-      validationsResult.push(await this.queryValidation.validate({ stockSymbol: stock }))
-    }
+    const validationsResult: Array<Either<Error, null>> = [
+      await this.paransValidation.validate(httpRequest.params),
+      await this.queryValidation.validate(httpRequest.query)
+    ]
     for (const validation of validationsResult) {
       if (validation.isLeft()) {
         return badRequest(validation.value)
       }
     }
+    const stockSymbol = httpRequest.params.stockSymbol
+    const stocksToCompare = httpRequest.query.stocksToCompare as string
+    let stocksToCompareSplit: string[] = []
+    if (stocksToCompare.includes(',')) {
+      stocksToCompareSplit = stocksToCompare.split(',')
+    }
+    await this.fetchStockComparison.perform({
+      stockSymbol,
+      stocksToCompare: (stocksToCompareSplit.length > 0) ? stocksToCompareSplit : [stocksToCompare]
+    })
     return { statusCode: 0, body: '' }
   }
 }
