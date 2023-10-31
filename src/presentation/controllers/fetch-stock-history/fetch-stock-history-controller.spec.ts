61e9b54cab1aedc586df258609aebe6c70d558cc
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:20:09 -0300

    test: ensure FetchStockHistoryController return 500 if Validation throws

--- src/presentation/controllers/fetch-stock-history/fetch-stock-history-controller.spec.ts
+++ src/presentation/controllers/fetch-stock-history/fetch-stock-history-controller.spec.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import { left, right, type Either } from '@/shared/either'
 import type { Validation } from '../../contracts'
 import type { HttpRequest } from '../../http-types/http'
@@ -51,5 +51,14 @@ describe('FetchStockHistory Controller', () => {
     )
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new Error('any_message')))
+  })
+
+  it('Should return 500 if Validation throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
