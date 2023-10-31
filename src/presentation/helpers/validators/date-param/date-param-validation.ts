046321a3b6995741d4c272e8c71e7816203ddc29
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:58:31 -0300

    feat: ensure DateParamValidation return InvalidDateError for date with invalid format (nonexistent date)

--- src/presentation/helpers/validators/date-param/date-param-validation.ts
+++ src/presentation/helpers/validators/date-param/date-param-validation.ts
@@ -4,10 +4,27 @@ import { right, type Either, left } from '@/shared/either'
 
 export class DateParamValidation implements Validation {
   async validate (input: any): Promise<Either<Error, null>> {
-    const datePattern = /^\d{4}-\d{2}-\d{2}$/
-    if (!datePattern.test(input.to)) {
+    if (!this.isValidDate(input.to)) {
       return left(new InvalidDateError(input.to))
     }
     return right(null)
+  }
+
+  private isValidDate (inputDate: string): boolean {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/
+    if (!dateRegex.test(inputDate)) {
+      return false
+    }
+    const parts = inputDate.split('-')
+    const year = parseInt(parts[0], 10)
+    const month = parseInt(parts[1], 10)
+    const day = parseInt(parts[2], 10)
+    if (month < 1 || month > 12) {
+      return false
+    }
+    if (day < 1 || day > new Date(year, month, 0).getDate()) {
+      return false
+    }
+    return true
   }
 }
