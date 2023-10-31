25e2d8c62e012112999a70eda3b705afb3ec1b64
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 11:33:12 -0300

    feat: ensure StocksHistory Route return 200 if StockHistory on success

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -1,9 +1,13 @@
 import type { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { fetchStockQuoteControllerFactory } from '../factories/controllers/fetch-stock-quote-controller-factory'
+import { fetchStockStoryControllerFactory } from '../factories/controllers/fetch-stock-history/fetch-stock-history-controller-factory'
 
 export default (router: Router): void => {
   router.get(
     '/stock/:stockSymbol/quote', adaptRoute(fetchStockQuoteControllerFactory())
+  )
+  router.get(
+    '/stocks/:stockSymbol/history', adaptRoute(fetchStockStoryControllerFactory())
   )
 }
