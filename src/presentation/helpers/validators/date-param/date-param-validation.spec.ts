34f600ae77a9be6bd1a1cd591829fedeb5637be7
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 08:00:20 -0300

    refactor: remove expect(result.isLeft()).toBe(true)

--- src/presentation/helpers/validators/date-param/date-param-validation.spec.ts
+++ src/presentation/helpers/validators/date-param/date-param-validation.spec.ts
@@ -15,49 +15,42 @@ describe('DateParam Validation', () => {
   it('Should return InvalidDateError for date with invalid format (non-dash separator)', async () => {
     const sut = makeSut()
     const result = await sut.validate({ to: '2023_01_02' })
-    expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023_01_02'))
   })
 
   it('Should return InvalidDateError for date with invalid format (non-numeric characters)', async () => {
     const sut = makeSut()
     const result = await sut.validate({ to: '2023-Jan-02' })
-    expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023-Jan-02'))
   })
 
   it('Should return InvalidDateError for date with invalid format (missing day)', async () => {
     const sut = makeSut()
     const result = await sut.validate({ to: '2023-01' })
-    expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023-01'))
   })
 
   it('Should return InvalidDateError for date with invalid format (missing month)', async () => {
     const sut = makeSut()
     const result = await sut.validate({ to: '2023--02' })
-    expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023--02'))
   })
 
   it('Should return InvalidDateError for date with invalid format (extra characters)', async () => {
     const sut = makeSut()
     const result = await sut.validate({ to: '2023-01-02-extra' })
-    expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023-01-02-extra'))
   })
 
   it('Should return InvalidDateError for an invalid date (nonexistent date)', async () => {
     const sut = makeSut()
     const result = await sut.validate({ to: '2023-02-30' })
-    expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023-02-30'))
   })
 
   it('Should return InvalidDateError for an invalid date (nonexistent date 2)', async () => {
     const sut = makeSut()
     const result = await sut.validate({ to: '2023-04-31' })
-    expect(result.isLeft()).toBe(true)
     expect(result.value).toEqual(new InvalidDateError('2023-04-31'))
   })
 })
