e0f8ff1bbb93bab9e0dedd342dd8a93801c3c788
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:51:01 -0300

    test: ensure DateParamValidation return InvalidDateError if date is invalid format

---
+++ src/presentation/helpers/validators/date-param/date-param-validation.spec.ts
@@ -0,0 +1,14 @@
+import { InvalidDateError } from '@/presentation/errors/invalid-date-error'
+import { DateParamValidation } from './date-param-validation'
+
+const makeSut = (): DateParamValidation => {
+  return new DateParamValidation()
+}
+
+describe('DateParam Validation', () => {
+  it('Should return InvalidDateError for date with invalid format', async () => {
+    const sut = makeSut()
+    const result = await sut.validate({ to: '2023/01/02' })
+    expect(result.value).toEqual(new InvalidDateError('2023/01/02'))
+  })
+})
