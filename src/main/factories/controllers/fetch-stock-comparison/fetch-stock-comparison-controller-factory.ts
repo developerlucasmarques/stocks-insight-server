689d78d56f5aea817549f4b5a0e2ac963512c16f
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 20:28:50 -0300

    feat: create FetchStockComparisonController Factory

---
+++ src/main/factories/controllers/fetch-stock-comparison/fetch-stock-comparison-controller-factory.ts
@@ -0,0 +1,14 @@
+import { logControllerDecoratorFactory } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { stockSymbolValidationFactory, stockToCompareValidationFactory } from '@/main/factories/validators'
+import type { Controller } from '@/presentation/contracts'
+import { FetchStockComparisonController } from '@/presentation/controllers'
+import { fetchStockComparasionUseCaseFactory } from '../../usecases'
+
+export const FetchStockComparisonControllerFactory = (): Controller => {
+  const controller = new FetchStockComparisonController(
+    stockSymbolValidationFactory(),
+    stockToCompareValidationFactory(),
+    fetchStockComparasionUseCaseFactory()
+  )
+  return logControllerDecoratorFactory(controller)
+}
--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,1 +1,2 @@
 export * from './fetch-stock-quote/fetch-stock-quote-controller'
+export * from './fetch-stock-comparison/fetch-stock-comparison-controller'
