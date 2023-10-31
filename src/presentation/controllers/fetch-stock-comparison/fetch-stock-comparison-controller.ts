b8ca40abc0e238b269116aa6b5ba79ab8d69e80a
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 15:06:44 -0300

    feat: ensure FetchStockComparisonController call Query Validation with correct values

--- src/presentation/controllers/fetch-stock-comparison/fetch-stock-comparison-controller.ts
+++ src/presentation/controllers/fetch-stock-comparison/fetch-stock-comparison-controller.ts
@@ -1,16 +1,21 @@
 import { badRequest } from '@/presentation/helpers/http/http-helper'
-import type { Controller, Validation } from '../../contracts'
-import type { HttpRequest, HttpResponse } from '../../http-types/http'
+import type { Controller, Validation } from '@/presentation/contracts'
+import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class FetchStockComparisonController implements Controller {
   constructor (
-    private readonly paransValidation: Validation
+    private readonly paransValidation: Validation,
+    private readonly queryValidation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const paramsValidationResult = await this.paransValidation.validate(httpRequest.params)
     if (paramsValidationResult.isLeft()) {
       return badRequest(paramsValidationResult.value)
+    }
+    const stocksToCompare: string[] = httpRequest.query?.stocksToCompare.split(',')
+    for (const stock of stocksToCompare) {
+      await this.queryValidation.validate({ stockSymbol: stock })
     }
     return { statusCode: 0, body: '' }
   }
