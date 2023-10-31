8bcb5f8f27727c419ecde43808e210048f3ff6c3
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 07:51:47 -0300

    test: ensure DateParamValidation return InvalidDateError for date with invalid format (non-dash separator)

--- src/presentation/helpers/validators/date-param/date-param-validation.spec.ts
+++ src/presentation/helpers/validators/date-param/date-param-validation.spec.ts
@@ -11,4 +11,11 @@ describe('DateParam Validation', () => {
     const result = await sut.validate({ to: '2023/01/02' })
     expect(result.value).toEqual(new InvalidDateError('2023/01/02'))
   })
+
+  it('Should return InvalidDateError for date with invalid format (non-dash separator)', async () => {
+    const sut = makeSut()
+    const result = await sut.validate({ to: '2023_01_02' })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toEqual(new InvalidDateError('2023_01_02'))
+  })
 })
