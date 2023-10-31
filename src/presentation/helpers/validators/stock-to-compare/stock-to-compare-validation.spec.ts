0a4b7a00f48c6967463c938b03b9dc9240b9c919
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 16:16:36 -0300

    test: ensure StockToCompareValidation return InvalidStockSymbolError if any FetchStockSymbolCache returns null

--- src/presentation/helpers/validators/stock-to-compare/stock-to-compare-validation.spec.ts
+++ src/presentation/helpers/validators/stock-to-compare/stock-to-compare-validation.spec.ts
@@ -1,5 +1,6 @@
 import type { FetchStockSymbolCache, StockSymbol } from '@/interactions/contracts/cache'
 import { StockToCompareValidation } from './stock-to-compare-validation'
+import { InvalidStockSymbolError } from '@/presentation/errors'
 
 const makeInput = (): any => ({
   stocksToCompare: 'any_stock,another_stock,other_stock'
@@ -26,12 +27,21 @@ const makeSut = (): SutTypes => {
 }
 
 describe('StockToCompare Validation', () => {
-  it('Shoulad call FetchStockSymbolCache with correct values', async () => {
+  it('Should call FetchStockSymbolCache with correct values', async () => {
     const { sut, fetchStockSymbolCacheStub } = makeSut()
     const fetchSpy = jest.spyOn(fetchStockSymbolCacheStub, 'fetchOneSymbol')
     await sut.validate(makeInput())
     expect(fetchSpy).toHaveBeenCalledWith('any_stock')
     expect(fetchSpy).toHaveBeenCalledWith('another_stock')
     expect(fetchSpy).toHaveBeenCalledWith('other_stock')
+  })
+
+  it('Should return InvalidStockSymbolError if any FetchStockSymbolCache returns null', async () => {
+    const { sut, fetchStockSymbolCacheStub } = makeSut()
+    jest.spyOn(fetchStockSymbolCacheStub, 'fetchOneSymbol').mockReturnValueOnce(
+      Promise.resolve(null)
+    )
+    const result = await sut.validate(makeInput())
+    expect(result.value).toEqual(new InvalidStockSymbolError('any_stock'))
   })
 })
