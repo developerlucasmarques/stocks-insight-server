83a75df60406932efa1624404189cc08ad88997b
mlucasdev <lucasmsantos819@gmail.com>
2023-11-01 09:43:08 -0300

    feat: ensure FetchStockGainsController call FetchStockGains with correct values

--- src/presentation/controllers/fetch-stock-gains/fetch-stock-gains-controller.ts
+++ src/presentation/controllers/fetch-stock-gains/fetch-stock-gains-controller.ts
@@ -1,9 +1,13 @@
+import type { FetchStockGains } from '@/domain/contracts'
 import type { Controller, Validation } from '@/presentation/contracts'
 import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class FetchStockGainsController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly fetchStockGains: FetchStockGains
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -11,6 +15,10 @@ export class FetchStockGainsController implements Controller {
       if (validationResult.isLeft()) {
         return badRequest(validationResult.value)
       }
+      const { stockSymbol, purchasedAt, purchasedAmount } = httpRequest.params
+      await this.fetchStockGains.perform({
+        stockSymbol, purchasedAt, purchasedAmount: Number(purchasedAmount)
+      })
       return await Promise.resolve({
         body: '', statusCode: 0
       })
