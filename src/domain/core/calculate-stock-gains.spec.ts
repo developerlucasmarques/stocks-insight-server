53cd726776cef081baa2629489f9da8b2545ab38
mlucasdev <lucasmsantos819@gmail.com>
2023-10-31 23:27:47 -0300

    test: ensure CalculateStockGains return an negative capital gains

--- src/domain/core/calculate-stock-gains.spec.ts
+++ src/domain/core/calculate-stock-gains.spec.ts
@@ -8,8 +8,17 @@ const makeFakeStockQuote = (): CalculateStockGainsData => ({
 })
 
 describe('CalculateStockGains', () => {
-  it('Should return an apital gains', async () => {
+  it('Should return an CapitalGains', async () => {
     const sut = CalculateStockGains.execute(makeFakeStockQuote())
     expect(sut.capitalGains).toBe(113.86)
+  })
+
+  it('Should return an negative capital gains', async () => {
+    const sut = CalculateStockGains.execute({
+      lastPrice: 150.99,
+      pricedAtDate: 160.00,
+      purchasedAmount: 2000
+    })
+    expect(sut.capitalGains).toBe(-188.12)
   })
 })
