70c14eaa294c228bba1efdb63c5d031eb7a7b4d2
mlucasdev <lucasmsantos819@gmail.com>
2023-11-01 09:00:19 -0300

    test: ensure AlphaVantageApi in fetchStockQuoteAtDate() return null if AlphaVantage return "Time Series (Daily)" but empty

--- src/infra/api/alpha-vantage/alpha-vantage-api.spec.ts
+++ src/infra/api/alpha-vantage/alpha-vantage-api.spec.ts
@@ -296,5 +296,14 @@ describe('AlphaVantageApi', () => {
       const result = await sut.fetchStockQuoteAtDate(makeFetchStockQuoteAtDateData())
       expect(result).toBeNull()
     })
+
+    it('Should return null if AlphaVantage return "Time Series (Daily)" but empty', async () => {
+      const sut = makeSut()
+      axiosMock.onGet(stockQuoteAtDateUrl).reply(200, {
+        'Time Series (Daily)': {}
+      })
+      const result = await sut.fetchStockQuoteAtDate(makeFetchStockQuoteAtDateData())
+      expect(result).toBeNull()
+    })
   })
 })
