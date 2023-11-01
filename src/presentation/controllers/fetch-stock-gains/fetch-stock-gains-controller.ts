3285ff4fb8a1fa053a937cc37a0f49969a33a914
mlucasdev <lucasmsantos819@gmail.com>
2023-11-01 09:34:16 -0300

    feat: ensure FetchStockGainsController return 500 if Validation throws

--- src/presentation/controllers/fetch-stock-gains/fetch-stock-gains-controller.ts
+++ src/presentation/controllers/fetch-stock-gains/fetch-stock-gains-controller.ts
@@ -1,17 +1,21 @@
 import type { Controller, Validation } from '@/presentation/contracts'
-import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class FetchStockGainsController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validationResult = await this.validation.validate(httpRequest.params)
-    if (validationResult.isLeft()) {
-      return badRequest(validationResult.value)
+    try {
+      const validationResult = await this.validation.validate(httpRequest.params)
+      if (validationResult.isLeft()) {
+        return badRequest(validationResult.value)
+      }
+      return await Promise.resolve({
+        body: '', statusCode: 0
+      })
+    } catch (error: any) {
+      return serverError(error)
     }
-    return await Promise.resolve({
-      body: '', statusCode: 0
-    })
   }
 }
