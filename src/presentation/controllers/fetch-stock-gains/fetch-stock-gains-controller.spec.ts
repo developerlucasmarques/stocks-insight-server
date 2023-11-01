0652a930987a37a05bf2a1d3061a11769f278912
mlucasdev <lucasmsantos819@gmail.com>
2023-11-01 09:45:51 -0300

    test: ensure FetchStockGainsController return 500 if FetchStockGains throws

--- src/presentation/controllers/fetch-stock-gains/fetch-stock-gains-controller.spec.ts
+++ src/presentation/controllers/fetch-stock-gains/fetch-stock-gains-controller.spec.ts
@@ -101,4 +101,13 @@ describe('FetchStockGains Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(notFound(new Error('any_message')))
   })
+
+  it('Should return 500 if FetchStockGains throws', async () => {
+    const { sut, fetchStockGainsStub } = makeSut()
+    jest.spyOn(fetchStockGainsStub, 'perform').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
