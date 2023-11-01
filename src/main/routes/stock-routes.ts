1f53c51536602e459fd03f66562dc897fdb3a957
mlucasdev <lucasmsantos819@gmail.com>
2023-11-01 13:23:18 -0300

    feat: ensure /stocks/:stockSymbol/gains route return 200 if StockGains on success

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -2,6 +2,7 @@ import type { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { fetchStockQuoteControllerFactory, fetchStockStoryControllerFactory } from '../factories/controllers'
 import { fetchStockComparisonControllerFactory } from '../factories/controllers/fetch-stock-comparison/fetch-stock-comparison-controller-factory'
+import { fetchStockGainsControllerFactory } from '../factories/controllers/fetch-stock-gains/fetch-stock-gains-factory'
 
 export default (router: Router): void => {
   router.get(
@@ -12,5 +13,8 @@ export default (router: Router): void => {
   )
   router.get(
     '/stocks/:stockSymbol/compare', adaptRoute(fetchStockComparisonControllerFactory())
+  )
+  router.get(
+    '/stocks/:stockSymbol/gains', adaptRoute(fetchStockGainsControllerFactory())
   )
 }
