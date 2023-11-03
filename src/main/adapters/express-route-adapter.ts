f398e79adcd9b6f8a8cf85c4f812a8123e1edeb2
mlucasdev <lucasmsantos819@gmail.com>
2023-11-03 00:32:30 -0300

    feat: add query request in params request

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -4,14 +4,14 @@ import type { Request, Response } from 'express'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    if (req.params?.stockSymbol) {
-      req.params.stockSymbol = req.params.stockSymbol.toUpperCase()
+    const params: Record<string, any> = req.params
+    if (req.query) {
+      const keys = Object.keys(req.query)
+      for (const key of keys) {
+        params[key] = req.query[key]
+      }
     }
-    const httpRequest: HttpRequest = {
-      body: req.body,
-      params: req.params,
-      query: req.query
-    }
+    const httpRequest: HttpRequest = { body: req.body, params }
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
