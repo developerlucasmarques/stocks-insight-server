1e6c6c3632b6096ad8e5bbbac1d2621315c9581e
mlucasdev <lucasmsantos819@gmail.com>
2023-11-03 02:33:59 -0300

    feat: ensure that stock symbols are converted to uppercase

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -1,20 +1,34 @@
 import type { Router } from 'express'
-import { adaptRoute } from '../adapters/express-route-adapter'
 import { fetchStockQuoteControllerFactory, fetchStockStoryControllerFactory } from '../factories/controllers'
 import { fetchStockComparisonControllerFactory } from '../factories/controllers/fetch-stock-comparison/fetch-stock-comparison-controller-factory'
 import { fetchStockGainsControllerFactory } from '../factories/controllers/fetch-stock-gains/fetch-stock-gains-factory'
+import { adaptMiddleware, adaptRoute } from '../adapters'
+import { converToUppercaseMiddlewareFactory } from '../factories/middlewares/convert-to-uppercase-middleware'
+
+const convertToUppercaseStockSymbolMiddleware = adaptMiddleware(
+  converToUppercaseMiddlewareFactory('params', 'stockSymbol')
+)
 
 export default (router: Router): void => {
   router.get(
-    '/stock/:stockSymbol/quote', adaptRoute(fetchStockQuoteControllerFactory())
+    '/stock/:stockSymbol/quote',
+    convertToUppercaseStockSymbolMiddleware,
+    adaptRoute(fetchStockQuoteControllerFactory())
   )
   router.get(
-    '/stocks/:stockSymbol/history', adaptRoute(fetchStockStoryControllerFactory())
+    '/stocks/:stockSymbol/history',
+    convertToUppercaseStockSymbolMiddleware,
+    adaptRoute(fetchStockStoryControllerFactory())
   )
   router.get(
-    '/stocks/:stockSymbol/compare', adaptRoute(fetchStockComparisonControllerFactory())
+    '/stocks/:stockSymbol/compare',
+    convertToUppercaseStockSymbolMiddleware,
+    adaptMiddleware(converToUppercaseMiddlewareFactory('params', 'stocksToCompare')),
+    adaptRoute(fetchStockComparisonControllerFactory())
   )
   router.get(
-    '/stocks/:stockSymbol/gains', adaptRoute(fetchStockGainsControllerFactory())
+    '/stocks/:stockSymbol/gains',
+    convertToUppercaseStockSymbolMiddleware,
+    adaptRoute(fetchStockGainsControllerFactory())
   )
 }
