d6ec68c07f0970e00a39672fae09847559aadd52
mlucasdev <lucasmsantos819@gmail.com>
2023-11-03 01:13:59 -0300

    test: ensure FetchStockHistoryControllerValidation Factory call ValidationComposite with all validations

--- src/main/factories/controllers/fetch-stock-history/fetch-stock-history-controller-validation-factory.spec.ts
+++ src/main/factories/controllers/fetch-stock-history/fetch-stock-history-controller-validation-factory.spec.ts
@@ -1,15 +1,16 @@
 import type { Validation } from '@/presentation/contracts'
+import { dateFormatValidationFactory, dateRangeValidationFactory, stockSymbolValidationFactory } from '@/main/factories/validators'
+import { fetchStockHistoryControllerValidationFactory } from './fetch-stock-history-controller-validation-factory'
 import { ValidationComposite } from '@/presentation/helpers/validators/composite/validation-composite'
-import { dateFormatValidationFactory, dateRangeValidationFactory } from '@/main/factories/validators'
-import { fetchStockHistoryQueryValidationFactory } from './fetch-stock-history-query-validation-factory'
 
 jest.mock('@/presentation/helpers/validators/composite/validation-composite')
 
-describe('FetchStockHistoryQueryValidation Factory', () => {
+describe('FetchStockHistoryControllerValidation Factory', () => {
   it('Should call ValidationComposite with all validations', () => {
-    fetchStockHistoryQueryValidationFactory()
+    fetchStockHistoryControllerValidationFactory()
     const validations: Validation[] = []
     validations.push(
+      stockSymbolValidationFactory(),
       dateFormatValidationFactory(['from', 'to']),
       dateRangeValidationFactory('from', 'to')
     )
