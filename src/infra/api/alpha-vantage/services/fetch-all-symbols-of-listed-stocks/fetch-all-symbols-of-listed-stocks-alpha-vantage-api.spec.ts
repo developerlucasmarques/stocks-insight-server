16c97e4b3e5214e36d118beb7e92b4046768fa94
mlucasdev <lucasmsantos819@gmail.com>
2023-11-02 22:33:19 -0300

    test: ensure FetchAllSymbolsOfListedStocksAlphaVantageApi return all Stock Symbols if listing status is a success

--- src/infra/api/alpha-vantage/services/fetch-all-symbols-of-listed-stocks/fetch-all-symbols-of-listed-stocks-alpha-vantage-api.spec.ts
+++ src/infra/api/alpha-vantage/services/fetch-all-symbols-of-listed-stocks/fetch-all-symbols-of-listed-stocks-alpha-vantage-api.spec.ts
@@ -14,8 +14,8 @@ const makeFakeListingStatusResponse = (): string => (
   'symbol,name,exchange,assetType,ipoDate,delistingDate,status\r\n' +
   'A,Agilent Technologies Inc,NYSE,Stock,1999-11-18,null,Active\r\n' +
   'AA,Alcoa Corp,NYSE,Stock,2016-10-18,null,Active\r\n' +
-  'AAA,AXS First Priority CLO Bond ETF,NYSE ARCA,ETF,2020-09-09,null,Active\r\n' +
-  'AAAU,Goldman Sachs Physical Gold ETF,BATS,ETF,2018-08-15,null,Active\r\n'
+  'ANY,AXS First Priority CLO Bond ETF,NYSE ARCA,ETF,2020-09-09,null,Active\r\n' +
+  'ANOTHER,Goldman Sachs Physical Gold ETF,BATS,ETF,2018-08-15,null,Active\r\n'
 )
 
 const makeSut = (): FetchAllSymbolsOfListedStocksAlphaVantageApi => {
@@ -60,5 +60,12 @@ describe('FetchAllSymbolsOfListedStocks AlphaVantageApi', () => {
     const promise = sut.fetchAll()
     await expect(promise).rejects.toThrow()
     await expect(promise).rejects.toBeInstanceOf(MaximumLimitReachedError)
+  })
+
+  it('Should return all Stock Symbols if listing status is a success', async () => {
+    const sut = makeSut()
+    axiosMock.onGet(makeFakeUrl()).reply(200, makeFakeListingStatusResponse())
+    const result = await sut.fetchAll()
+    expect(result).toEqual(['A', 'AA', 'ANY', 'ANOTHER'])
   })
 })
