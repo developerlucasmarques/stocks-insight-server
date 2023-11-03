f65d0b38214387ffb538bb4c7ac2e0fad2dc1621
mlucasdev <lucasmsantos819@gmail.com>
2023-11-02 21:56:21 -0300

    refactor: remove AlphaVantageApi Class and AlphaVantageApi Factory

--- src/main/factories/api/alpha-vantage-api-factory.ts
+++
@@ -1,6 +0,0 @@
-import { AlphaVantageApi } from '@/infra/api/alpha-vantage/alpha-vantage-api'
-import env from '@/main/config/env'
-
-export const alphaVantageApiFactory = (): AlphaVantageApi => {
-  return new AlphaVantageApi(env.alphaVantageApiKey)
-}
